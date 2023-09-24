c4b1c828e4962c6ad443e5c81eff19b7f77073d5
Pratyush Saxena <105998606+pratyushsaxena1@users.noreply.github.com>
2023-09-24 15:23:01 -0400

    Update sendemailcheck.php

--- HTML & PHP/sendemailcheck.php
+++ HTML & PHP/sendemailcheck.php
@@ -8,7 +8,7 @@
             $emailFrom = 'w3study.contact@gmail.com';
             $emailSubject = 'Confirm w3-Study Sign Up';
             $emailTo = $_POST['email'];
-            $headers = "From: webmaster@example.com" . "\r\n";
+            $headers = "From: w3-Study" . "\r\n";
             $headers .= "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
             $body = '<a href="http://w3-study.com/html/insertlogin.php">Click here to verify your account.</a>';
